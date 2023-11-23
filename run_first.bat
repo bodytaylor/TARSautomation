1b4807e06b6f0365c67250682a745e893d99405a
Nantawat Sangkarn <taylor.nobodyclub@gmail.com>
2023-11-23 14:24:58 +0700

    Create run_first.bat

---
+++ run_first.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Setting up virtual environment...
+
+:: Set the path where you want to create the virtual environment
+set VENV_FOLDER=.venv
+
+:: Check if the virtual environment folder already exists
+if not exist %VENV_FOLDER% (
+    python -m venv %VENV_FOLDER%
+    echo Virtual environment created.
+) else (
+    echo Virtual environment already exists.
+)
+
+:: Activate the virtual environment
+call %VENV_FOLDER%\Scripts\activate
+
+echo Installing required packages...
+:: Install your required packages using pip
+pip install -r requirements.txt
+
+echo Setup complete.
+pause
