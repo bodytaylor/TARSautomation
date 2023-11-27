d7f21e61bcf5988bac0568756bb64588d506349e
Nantawat Sangkarn <taylor.nobodyclub@gmail.com>
2023-11-27 16:12:21 +0700

    Update dummy.py

--- code/dummy.py
+++ code/dummy.py
@@ -1,4 +1,51 @@
-from datetime import date
-    
-today = date.today().strftime("%d%m%Y")
-print(today)
+# Load environment variables from .env file
+def user_credential():
+    load_dotenv()
+
+    # Access the variables using os.environ.get()
+    username = os.environ.get("TARSUSER")
+    password = os.environ.get("PASSWORD")
+
+    # Check if .env file exists
+    if not (username and password):
+        print("No .env file found. Please provide your credentials:")
+        username = input("Username: ")
+        password = input("Password: ")
+
+        # Save the credentials to a new .env file
+        with open(".env", "w") as env_file:
+            env_file.write(f"TARSUSER={username}\n")
+            env_file.write(f"PASSWORD={password}\n")
+
+        print(".env file created with provided credentials.")
+    else:
+        print(f"Credentials loaded from .env file. Username: {username}")
+        
+    return username, password
+
+def login(username, password):
+        # Navigate to the login page
+    driver.get("https://dataweb.accor.net/dotw-trans/login!input.action")
+
+    # Wait for an element to be visible
+    try:
+        username_field = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.NAME, "login"))
+        )
+            # Find the username and password input fields and enter your credentials
+        username_field = driver.find_element(By.ID, "loginField")
+        password_field = driver.find_element(By.NAME, "password")
+
+
+        driver.execute_script("arguments[0].value = '';", username_field)
+        username_field.send_keys(username)
+        driver.execute_script("arguments[0].value = arguments[1];", password_field, password)
+
+        # Submit the login form
+        submit_button = driver.find_element(By.CSS_SELECTOR, 'input#login_0[value="Submit"].submit')
+
+        # Click the button
+        submit_button.click()
+        password_field.send_keys(Keys.RETURN)
+    except ValueError as e:
+        print(e)
